bf62ba4243f160144e1b32df0911d53dc082af84
DebasmitaSahu94 <debasmitasahu.msg@gmail.com>
2026-05-26 23:23:29 +0530

    Add additional Azure resource groups

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "rg"{
   name = "testrg"
   location = "eastus"
 }
+resource "azurerm_resource_group" "rg"{
+  name = "testrg2"
+  location = "eastus"
+}
+resource "azurerm_resource_group" "rg"{
+  name = "testrg3"
+  location = "eastus"
+}
